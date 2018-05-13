11ab8b794ce43e0c4dcb8a80270f7926b4b09294
Payton Trosclair <pjtrosclair@ksu.edu>
2018-05-13 00:44:51 -0500

    added abstracts

---
+++ GameSol/GameSol/Piece.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSol
+{
+    abstract class Piece
+    {
+        public Tuple<int, int> rotationAxis;
+    }
+}
